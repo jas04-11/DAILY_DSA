77c6afbe04dc6994cc22c0ee57b5111419f64d98
Jasleen Kaur <140260520+jas04-11@users.noreply.github.com>
2026-07-30 20:47:58 +0530

    Update MoveZeroes.java

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,3 +1,12 @@
+/*Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ */
 class Solution {
     public void moveZeroes(int[] nums) {
        int start=0;
